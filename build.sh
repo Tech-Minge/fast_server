7acbc58ae4073f3e1f62f06ade6a83b69305844a
yangminqi <1064883993@qq.com>
2025-07-01 08:41:58 +0800

    first

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# 如果没有build目录，创建该目录
+if [ ! -d `pwd`/build ]; then
+    mkdir `pwd`/build
+fi
+
+rm -rf `pwd`/build/*
+
+cd `pwd`/build &&
+    cmake .. &&
+    make
+
+
---
+++ build/src/CMakeFiles/mymuduo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../lib/libmymuduo.pdb"
+  "../../lib/libmymuduo.so"
+  "CMakeFiles/mymuduo.dir/InetAddress.cpp.o"
+  "CMakeFiles/mymuduo.dir/InetAddress.cpp.o.d"
+  "CMakeFiles/mymuduo.dir/Socket.cpp.o"
+  "CMakeFiles/mymuduo.dir/Socket.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mymuduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
